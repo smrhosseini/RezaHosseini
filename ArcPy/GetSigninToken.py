2ed51844439ffedd6b9bf94b2141af1952587de0
smrhosseini <56230851+smrhosseini@users.noreply.github.com>
2022-01-05 13:08:17 +0330

    Add files via upload

--- ArcPy/GetSigninToken.py
+++ ArcPy/GetSigninToken.py
@@ -1,1 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  5 12:58:02 2022
 
+@author: Reza
+"""
+
+
+import arcpy
+
+token = arcpy.GetSigninToken()
+if token is not None:
+    print(token['token'])
--- ArcPy/ListPortalURLs.py
+++ ArcPy/ListPortalURLs.py
@@ -1,1 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  5 12:58:57 2022
 
+@author: Reza
+"""
+
+
+import arcpy
+
+# For example: ['http://testext.arcgis.com/', 'http://www.arcgis.com/']
+print(arcpy.ListPortalURLs())
