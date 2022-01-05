e04e28e818d1f4b86cdd7f0017e6ac6e22fb2e7f
smrhosseini <56230851+smrhosseini@users.noreply.github.com>
2022-01-05 13:24:18 +0330

    Add files via upload

--- ArcPy/ArcGIS Online or Portal/GetActivePortalURL.py
+++ ArcPy/ArcGIS Online or Portal/GetActivePortalURL.py
@@ -1,1 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  5 12:57:20 2022
 
+@author: Reza
+"""
+
+
+import arcpy
+
+# For example: 'http://www.arcgis.com/'
+print(arcpy.GetActivePortalURL())
